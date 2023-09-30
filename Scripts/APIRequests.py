0137348bfe50c38706a9e4112e212a834a8bb83b
eashan2480 <eashan.ghelani7@gmail.com>
2023-09-30 23:15:34 +0100

    SAVED image URL

--- Scripts/APIRequests.py
+++ Scripts/APIRequests.py
@@ -1,18 +1,58 @@
 from square.client import Client
+from pprint import pprint
+import csv
 
 client = Client(
-  access_token="EAAAEDVYe8ka9okFKiK3xkEb-sjcPyg9HNZwIjQDH5JX9EM3zXwQon8avcZgklNE",
-  environment="sandbox"
+    access_token="EAAAEDVYe8ka9okFKiK3xkEb-sjcPyg9HNZwIjQDH5JX9EM3zXwQon8avcZgklNE",
+    environment="sandbox"
 )
 
 
+with open("./Catalog/catalog.csv", 'w', newline='') as file:
+  writer = csv.writer(file)
+  writer.writerow(["ITEM ID GOES HERE", "ITEM NAME GOES HERE", "BLANK (WILL BE IMAGE URL) GOES HERE"])
+file.close()
+  
+  
+  # with open('./Data/MasterScore.csv', 'a', newline='') as file:
+  #           writer = csv.writer(file)
+  #           writer.writerow(["ITEM ID GOES HERE", "ITEM NAME GOES HERE", "BLANK (WILL BE IMAGE URL) GOES HERE"])
 
-result = client.catalog.retrieve_catalog_object(
-  object_id = "IN7HYUDZLRFPKXGRK4ZYS7ST",
-  include_related_objects = True
-)
 
-if result.is_success():
-  print(result.body)  
-elif result.is_error():
-  print(result.errors)
+try:
+  with open("./Catalog/catalog.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    prevCursor = ""
+    result = None
+    globalCountItems = 1
+    while True:
+      if prevCursor == "":
+        # Lists catalog for the first page (100 items)
+        result = client.catalog.list_catalog(
+          types="ITEM"
+        )
+      else:
+        # Lists the catalog for the next page as per cursor
+        result = client.catalog.list_catalog(
+          cursor=prevCursor,
+          types="ITEM"
+        )
+
+      # Loop through
+      if result.is_success():
+        for object in result.body["objects"]:
+          item_id = object["id"]
+          item_name = object["item_data"]["name"]
+          # print(globalCountItems, item_id, item_name)
+          writer.writerow([globalCountItems,item_id, item_name, ""])
+          globalCountItems += 1
+      elif result.is_error():
+        # TODO throw error
+        pprint(result.errors)
+
+      prevCursor = result.body["cursor"]
+except Exception as error:
+  file.close()
+  print(f"Error: {error}")
+
+
--- Scripts/ImageExtractor.py
+++ Scripts/ImageExtractor.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import random
+import numpy as np
+import csv
 
  
 # What you enter here will be searched for in
 # Google Images
-query = "jelly cat frog"
+
+with open('./Catalog/catalog.csv', 'r') as f:
+    reader = csv.reader(f)
+    rows = list(reader)
+
+numpyList = np.array(rows)
+f.close()
 
 options = webdriver.ChromeOptions()
 # This will disable any extensions installed in chrome (for testing)
@@ -26,24 +35,49 @@ time.sleep(1)
 reject_policy_button = driver.find_element(By.ID, 'W0wltc')
 reject_policy_button.click()
 
+# ----------------- RUNNING THE SEARCH FOR THE FIRST TIME ----------------------------------
+
+listOfNames = numpyList[:,2]
+query = listOfNames[0]
+listOfImageURLS = []
+
+
 # Finding the search box
 box = driver.find_element(By.NAME, 'q')
-
-
-
 # Type the search query in the search box
 box.send_keys(query)
 box.submit()
-
 time.sleep(1)
-
 # Only gets the first element found on the search
 firstImageReturned = driver.find_elements(by='css selector', value='.FRuiCf')[0]
 imageElement = firstImageReturned.find_element(By.TAG_NAME,'img')
-# Outputs image src
-print(imageElement.get_attribute('src'))
-print("done")
+listOfImageURLS.append(imageElement.get_attribute('src'))
+# TODO: Write IMAGE URL to CSV
 
+for i in range(1,len(listOfNames)):
+    try:
+        box = driver.find_element(By.NAME, 'q')
+        box.clear()
+        query = str(random.randint(0,50))
+        # Type the search query in the search box
+        query = listOfNames[i]
+        box.send_keys(query)
+        box.submit()
+        # time.sleep(0.25)
+        # Only gets the first element found on the search
+        firstImageReturned = driver.find_elements(by='css selector', value='.FRuiCf')[0]
+        imageElement = firstImageReturned.find_element(By.TAG_NAME,'img')
+        # print(imageElement.get_attribute('src'))
+        listOfImageURLS.append(imageElement.get_attribute('src'))
+    except Exception as e:
+        listOfImageURLS.append("")
+
+
+numpyList[:, 3] = listOfImageURLS
+formatted_data = np.array([f'{row[0]}, "{row[1]}", "{row[2]}", "{row[3]}"' for row in numpyList])
+# savedArray = np.asarray(numpyList)
+
+np.savetxt('./Catalog/ImageCatalog.csv', formatted_data, delimiter=',', fmt='%s', header='Item Number,Item ID,Item Name,Image URL', comments='')
 driver.close()
 driver.quit()
 
